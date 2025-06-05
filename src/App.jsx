f690aefff8a8cf86a8a9504f33257687eb1ee812
Umarolimjonov2009 <feruzashokirova1988@gmail.com>
2025-06-05 18:19:43 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Olimjonov Umar</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam totam perspiciatis neque rerum dolor! Culpa aspernatur, ullam debitis dolorum iusto maxime pariatur? Quos minima facilis quia suscipit voluptatem atque quae veritatis maxime aspernatur quo quisquam aperiam ipsum eligendi commodi in, accusantium, provident eum incidunt ipsa. Odio quidem suscipit, quas fuga tenetur natus explicabo expedita id.</p>
+      <ul>
+        <li>Html</li>
+        <li>css</li>
+        <li>script.js</li>
+        <li>react</li>
+        <li>react</li>
+        <li>react</li>
+      </ul>
+    </div>
+  )
+}
+
+export default App
